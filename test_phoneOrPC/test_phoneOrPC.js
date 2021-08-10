daa3bd46eab3e3769ae4b46b26fe10fcd48e63b3
funnysandwich <85061199+funnysandwich@users.noreply.github.com>
2021-08-10 23:08:14 +0800

    Add files via upload

---
+++ test_phoneOrPC/test_phoneOrPC.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(500, 500);
+  textSize(20);
+}
+
+
+function draw() {
+  background(0);
+  
+  fill(255);
+  text("hi, this is PC",100,100);
+}
---
+++ test_phoneOrPC/test_phoneOrPC_phone.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(500, 500);
+  textSize(20);
+}
+
+
+function draw() {
+  background(0);
+
+  fill(255, 128, 128);
+  text("hi, this is phone", 100, 100);
+}
