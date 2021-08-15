1647e5640f9bcb997604867f97f53f9b2d198c05
funnysandwich <85061199+funnysandwich@users.noreply.github.com>
2021-08-15 15:04:17 +0800

    Add files via upload

---
+++ test_touchEvent/test_touchEvent.js
@@ -0,0 +1,41 @@
+
+let y ;
+let y2, y2_flash;
+let img;
+
+function preload() {
+  img = loadImage("data/img_sideText01.png");
+}
+
+
+function setup() {
+  createCanvas(windowWidth, 500);
+  y = 0;
+  y2 = 0;
+  y2_flash = 0.0;
+}
+
+
+function draw() {
+  background(0);
+  fill(255);
+  text("movedY: "+movedY, 10, 10);
+  text("y: "+y, 10, 25);
+  text("mouseY: "+mouseY, 10, 40);
+  text("pmouseY: "+pmouseY, 10, 55);
+  text(mouseY - pmouseY, 10, 70);
+  text("y2: "+y2, 10, 85);
+
+
+  image(img, 0, y2, windowWidth, windowWidth*(img.height/img.width));
+  y2_flash += (0 - y2_flash) * 0.05;
+}
+
+
+function touchMoved() {
+  y += movedY;
+
+  y2_flash = mouseY - pmouseY;
+
+  y2 += y2_flash;
+}
