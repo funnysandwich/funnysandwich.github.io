0e8e54da99c1dbb0fdfdf4a9abc584c78a4e0545
funnysandwich <85061199+funnysandwich@users.noreply.github.com>
2021-08-18 01:46:32 +0800

    Update KDLAF2021_test05.js

--- KDLAF2021_test05/KDLAF2021_test05.js
+++ KDLAF2021_test05/KDLAF2021_test05.js
@@ -208,7 +208,7 @@ function setup() {
 
   open_info = false;
   
-    a = createDiv("hahahahaha<br>hahaha<br>halalallalalalalalalallaaaaaaaaaaa");
+    a = createDiv("<br>hahahahaha<br>hahaha<br>halalalaa");
 
   a.position(100, 100);
   a.style('width', '200');
