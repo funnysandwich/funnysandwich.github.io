afa53e8cd58a46787d2c7c3cd5384bd92133180a
funnysandwich <85061199+funnysandwich@users.noreply.github.com>
2022-05-07 15:13:26 +0800

    Update phoneTexture_220507_test01.js

--- phoneTexture_220507_test01/phoneTexture_220507_test01.js
+++ phoneTexture_220507_test01/phoneTexture_220507_test01.js
@@ -7,8 +7,8 @@ function setup() {
   textureMode(NORMAL);
 
   for (let i=0; i<P.length; i++) {
-    P[i] = createGraphics(375*(i*2+1), 375*(i+1)*2);
-    P[i].background(128);
+    P[i] = createGraphics(375*(i*2+1), 375*5);
+    P[i].background(160,60,0);
     P[i].fill(255, 0, 0);
     P[i].ellipse(P[i].width/2, P[i].height/2, 300, 300);
   }
