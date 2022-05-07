33d8b8e4515a24e4dbe483de26beae949d32c17b
funnysandwich <85061199+funnysandwich@users.noreply.github.com>
2022-05-07 14:49:03 +0800

    Add files via upload

---
+++ phoneTexture_220507_test01/phoneTexture_220507_test01.js
@@ -0,0 +1,34 @@
+let P = new Array(5);
+
+
+function setup() {
+  createCanvas(500, 500, WEBGL);
+  
+  textureMode(NORMAL);
+
+  for (let i=0; i<P.length; i++) {
+    P[i] = createGraphics(375, 375*(i+1));
+    P[i].background(128, 128, 255);
+    P[i].fill(255, 128, 128);
+    P[i].ellipse(P[i].width/2, P[i].height/2, 300, 300);
+  }
+}
+
+
+function draw() {
+  background(200);
+
+  for (let i=0; i<P.length; i++) {
+    let center_x = map(i, 0, P.length-1, -200, 200);
+    fill(255);
+    beginShape();
+    texture(P[i]);
+    vertex(center_x-40, -40, 0, 0, 0);
+    vertex(center_x+40, -40, 0, 1, 0);
+    vertex(center_x+40, 40, 0, 1, 1);
+    vertex(center_x-40, 40, 0, 0, 1);
+
+    endShape();
+    fill(255);
+  }
+}
