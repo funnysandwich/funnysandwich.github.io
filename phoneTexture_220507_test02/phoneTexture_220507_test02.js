5ddc5077124c2895ce3b8316fabcff6c8128f2e9
funnysandwich <85061199+funnysandwich@users.noreply.github.com>
2022-05-07 16:07:32 +0800

    Update phoneTexture_220507_test02.js

--- phoneTexture_220507_test02/phoneTexture_220507_test02.js
+++ phoneTexture_220507_test02/phoneTexture_220507_test02.js
@@ -35,21 +35,23 @@ function setup() {
   textureMode(NORMAL);
 
   for (let i=0; i<P.length; i++) {
-    let w = floor(map(i, 0, P.length, 1800, 2300));
-    let h = 375*5;    
-    P[i] = createGraphics(w, h);
-    P[i].background(128, 128, 255);
-    P[i].fill(255, 0, 0);
-    P[i].ellipse(P[i].width/2, P[i].height/2, 300, 300);
+    if (i == 4) {
+      let w = floor(map(i, 0, P.length, 1800, 2300));
+      let h = 375*5;    
+      P[i] = createGraphics(w, h);
+      P[i].background(128, 128, 255);
+      P[i].fill(255, 0, 0);
+      P[i].ellipse(P[i].width/2, P[i].height/2, 300, 300);
 
-    IM[i] = createImage(w, h);
-    IM[i].loadPixels();
-    for (let j = 0; j < IM[i].width; j++) {
-      for (let k = 0; k < IM[i].height; k++) {
-        IM[i].set(j, k, color(0, 90, 102));
+      IM[i] = createImage(w, h);
+      IM[i].loadPixels();
+      for (let j = 0; j < IM[i].width; j++) {
+        for (let k = 0; k < IM[i].height; k++) {
+          IM[i].set(j, k, P[i].get(j, k));
+        }
       }
+      IM[i].updatePixels();
     }
-    IM[i].updatePixels();
   }
 }
 
@@ -58,30 +60,32 @@ function draw() {
   background(200);
 
   for (let i=0; i<P.length; i++) {
-    let center_x = map(i, 0, P.length-1, -200, 200);
-    fill(255);
-    beginShape();
-    texture(P[i]);
-    vertex(center_x-40, -40, -1, 0, 0);
-    vertex(center_x+40, -40, -1, 1, 0);
-    vertex(center_x+40, 40, -1, 1, 1);
-    vertex(center_x-40, 40, -1, 0, 1);
+    if (i == 4) {
+      let center_x = map(i, 0, P.length-1, -200, 200);
+      fill(255);
+      beginShape();
+      texture(P[i]);
+      vertex(center_x-40, -40, -1, 0, 0);
+      vertex(center_x+40, -40, -1, 1, 0);
+      vertex(center_x+40, 40, -1, 1, 1);
+      vertex(center_x-40, 40, -1, 0, 1);
 
-    endShape();
-    fill(255);
+      endShape();
+      fill(255);
 
 
 
-    fill(255);
-    beginShape();
-    texture(IM[i]);
-    vertex(center_x-40, -140, -1, 0, 0);
-    vertex(center_x+40, -140, -1, 1, 0);
-    vertex(center_x+40, -60, -1, 1, 1);
-    vertex(center_x-40, -60, -1, 0, 1);
+      fill(255);
+      beginShape();
+      texture(IM[i]);
+      vertex(center_x-40, -140, -1, 0, 0);
+      vertex(center_x+40, -140, -1, 1, 0);
+      vertex(center_x+40, -60, -1, 1, 1);
+      vertex(center_x-40, -60, -1, 0, 1);
 
-    endShape();
-    fill(255);
+      endShape();
+      fill(255);
+    }
   }
   image(INFO, -250, -250, 500, 500);
 }
