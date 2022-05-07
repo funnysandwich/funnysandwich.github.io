2f96d77cdf882dbcb1a568fc57f7d6aa016af2b3
funnysandwich <85061199+funnysandwich@users.noreply.github.com>
2022-05-07 15:19:18 +0800

    Update phoneTexture_220507_test01.js

--- phoneTexture_220507_test01/phoneTexture_220507_test01.js
+++ phoneTexture_220507_test01/phoneTexture_220507_test01.js
@@ -1,13 +1,23 @@
 let P = new Array(5);
-
+let INFO;
 
 function setup() {
   createCanvas(500, 500, WEBGL);
+  INFO = createGraphics(500, 500);
+  INFO.background(0,0);
+  INFO.fill(0);
+  INFO.noStroke();
+  for(let i=0;i<5;i++){
+    let w = map(i,0,P.length,1800,3000);
+    let center_x = map(i, 0, P.length-1, -200, 200);
+    INFO.text("i:"+i+", "+nfc(w,2),center_x, 100);
+  }
   
   textureMode(NORMAL);
 
   for (let i=0; i<P.length; i++) {
-    P[i] = createGraphics(375*(i*2+1), 375*5);
+    let w = map(i,0,P.length,1800,3000);
+    P[i] = createGraphics(w, 375*5);
     P[i].background(160,60,0);
     P[i].fill(255, 0, 0);
     P[i].ellipse(P[i].width/2, P[i].height/2, 300, 300);
@@ -30,5 +40,7 @@ function draw() {
 
     endShape();
     fill(255);
+    
+    image(INFO,0,0,500,500);
   }
 }
